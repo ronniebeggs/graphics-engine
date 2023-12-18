1a2e13d63986b799fdf129b3a9a92dc82a48af26
Ronnie Beggs <rbeggs@berkeley.edu>
2023-12-17 18:59:00 -0800

    [wip] render proof of concept cube.

--- src/util/Mesh.java
+++ src/util/Mesh.java
@@ -3,12 +3,26 @@ import java.awt.Color;
 import java.util.List;
 
 public class Mesh {
-    public Color color;
-    public List<Coordinate> vertices;
+    private Color color;
+    private Coordinate[] vertices;
+    private int numVertices;
 
-    public Mesh(Color color, List<Coordinate> vertices) {
+    public Mesh(Coordinate[] vertices, Color color) {
         this.color = color;
         this.vertices = vertices;
+        this.numVertices = vertices.length;
+    }
+
+    public Coordinate[] getVertices() {
+        return vertices;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getNumVertices() {
+        return numVertices;
     }
 
 }
