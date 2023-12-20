b9b952ec2b3bac7fd4c1834a945a0270db859879
Ronnie Beggs <rbeggs@berkeley.edu>
2023-12-19 20:02:01 -0800

    [wip] attempt to implement fixed orbital rotations.

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -4,6 +4,7 @@ import edu.princeton.cs.algs4.StdDraw;
 import world.World;
 import world.Cube;
 import world.Camera;
+import world.Entity;
 
 /**
  * Class that handles the overarching operations of the project.
@@ -12,6 +13,7 @@ import world.Camera;
 public class Engine {
 
     Renderer ter = new Renderer();
+    Camera camera;
     public final int DISPLAY_WIDTH = 600;
     public final int DISPLAY_HEIGHT = 600;
     public final int VERTICAL_VIEW_ANGLE = 60;
@@ -21,7 +23,7 @@ public class Engine {
      * */
     public void singleFrameTest() {
         World world = new World();
-        Camera camera = new Camera(0, 0, 0);
+        camera = new Camera(0, 0, 0);
         Cube cube = new Cube(-50, -50, 100, 60);
         world.insertEntity(cube);
         ter.initialize(camera, DISPLAY_WIDTH, DISPLAY_HEIGHT, VERTICAL_VIEW_ANGLE);
@@ -30,7 +32,7 @@ public class Engine {
 
     public void mainLoop() {
         World world = new World();
-        Camera camera = new Camera(0, 0, 0);
+        camera = new Camera(0, 0, 0);
         Cube cube = new Cube(0, 0, 100, 60);
         world.insertEntity(cube);
         ter.initialize(camera, DISPLAY_WIDTH, DISPLAY_HEIGHT, VERTICAL_VIEW_ANGLE);
@@ -38,33 +40,65 @@ public class Engine {
             ter.renderFrame(world);
             if (StdDraw.hasNextKeyTyped()) {
                 char keyPress = StdDraw.nextKeyTyped();
-                switch (keyPress) {
-                    case 'w' -> {
-                        camera.moveFrontal(10);
-                    }
-                    case 's' -> {
-                        camera.moveFrontal(-10);
-                    }
-                    case 'd' -> {
-                        camera.moveLateral(10);
-                    }
-                    case 'a' -> {
-                        camera.moveLateral(-10);
-                    }
-                    case 'i' -> {
-                        camera.rotatePitch(-10);
-                    }
-                    case 'k' -> {
-                        camera.rotatePitch(10);
-                    }
-                    case 'l' -> {
-                        camera.rotateYaw(10);
-                    }
-                    case 'j' -> {
-                        camera.rotateYaw(-10);
-                    }
+                boolean isTargetLocked = true;
+                if (isTargetLocked) {
+                    fixedOrbitalMovement(cube, keyPress);
+                } else {
+                    freeMovement(keyPress);
                 }
             }
         }
+    }
+
+    public void fixedOrbitalMovement(Entity target, char keyPress) {
+        switch (keyPress) {
+            case 'w' -> {
+                camera.moveFrontal(10);
+            }
+            case 's' -> {
+                camera.moveFrontal(-10);
+            }
+            case 'd' -> {
+                camera.rotateAround(target, 10);
+            }
+            case 'a' -> {
+                camera.rotateAround(target, -10);
+            }
+        }
+    }
+
+    public void freeMovement(char keyPress) {
+        switch (keyPress) {
+            case 'w' -> {
+                camera.moveFrontal(10);
+            }
+            case 's' -> {
+                camera.moveFrontal(-10);
+            }
+            case 'd' -> {
+                camera.moveLateral(10);
+            }
+            case 'a' -> {
+                camera.moveLateral(-10);
+            }
+            case 'i' -> {
+                camera.rotatePitch(-10);
+            }
+            case 'k' -> {
+                camera.rotatePitch(10);
+            }
+            case 'l' -> {
+                camera.rotateYaw(10);
+            }
+            case 'j' -> {
+                camera.rotateYaw(-10);
+            }
+            case 'q' -> {
+                camera.moveTransverse(10);
+            }
+            case 'e' -> {
+                camera.moveTransverse(-10);
+            }
+        };
     }
 }
