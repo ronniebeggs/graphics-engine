ed358a94f2301863e0b811c0cc4749da0ab4617a
Ronnie Beggs <rbeggs@berkeley.edu>
2024-01-07 17:33:13 -0800

    [refactor] optimize mesh creation by limiting duplicate Coordinate creation.

--- src/world/Sphere.java
+++ src/world/Sphere.java
@@ -19,97 +19,29 @@ public class Sphere extends Entity {
         this.meshes = new ArrayList<>();
         createMesh();
     }
+    /** Create a UV Sphere surface mesh according to the inputted number of slices/stacks. */
     public void createMesh() {
-        Coordinate top = new Coordinate(xPosition, yPosition + radius, zPosition);
-
         double sliceAngle = (double) Math.TAU / numSlices;
         double stackAngle = (double) Math.PI / numStacks;
 
-        double phi;
-        double distanceFromYAxis;
-        double distanceFromXZPlane;
-        double y;
-
-        // create top triangles
-        phi = 1 * stackAngle;
-        distanceFromYAxis = radius * Math.sin(phi);
-        distanceFromXZPlane = radius * Math.cos(phi);
-        y = yPosition + distanceFromXZPlane;
-
-        for (int n = 0; n < numSlices; n++) {
-            double theta = n * sliceAngle;
-
-            double x0 = xPosition + distanceFromYAxis * Math.cos(theta);
-            double z0 = zPosition + distanceFromYAxis * Math.sin(theta);
-            Coordinate v0 = new Coordinate(x0, y, z0);
-
-            double x1 = xPosition + distanceFromYAxis * Math.cos(theta + sliceAngle);
-            double z1 = zPosition + distanceFromYAxis * Math.sin(theta + sliceAngle);
-            Coordinate v1 = new Coordinate(x1, y, z1);
-
-            Color meshColor = StdDraw.BOOK_BLUE;
-            if (n % 2 == 0) {
-                meshColor = StdDraw.BOOK_RED;
-            }
-
-            meshes.add(new Mesh(new Coordinate[]{top, v0, v1}, meshColor));
-        }
-
-
-        for (int n = 0; n < numSlices; n++) {
-            double theta = n * sliceAngle;
-            for (int m = 1; m < numStacks - 1; m++) {
-                phi = m * stackAngle;
-
-                double distanceFromYAxis0 = radius * Math.sin(phi);
-                double distanceFromXZPlane0 = radius * Math.cos(phi);
-                double y0 = yPosition + distanceFromXZPlane0;
-
-                double x00 = xPosition + distanceFromYAxis0 * Math.cos(theta);
-                double z00 = zPosition + distanceFromYAxis0 * Math.sin(theta);
-                Coordinate v00 = new Coordinate(x00, y0, z00);
-
-                double x01 = xPosition + distanceFromYAxis0 * Math.cos(theta + sliceAngle);
-                double z01 = zPosition + distanceFromYAxis0 * Math.sin(theta + sliceAngle);
-                Coordinate v01 = new Coordinate(x01, y0, z01);
-
-                double distanceFromYAxis1 = radius * Math.sin(phi + stackAngle);
-                double distanceFromXZPlane1 = radius * Math.cos(phi + stackAngle);
-                double y1 = yPosition + distanceFromXZPlane1;
-
-                double x10 = xPosition + distanceFromYAxis1 * Math.cos(theta);
-                double z10 = zPosition + distanceFromYAxis1 * Math.sin(theta);
-                Coordinate v10 = new Coordinate(x10, y1, z10);
-
-                double x11 = xPosition + distanceFromYAxis1 * Math.cos(theta + sliceAngle);
-                double z11 = zPosition + distanceFromYAxis1 * Math.sin(theta + sliceAngle);
-                Coordinate v11 = new Coordinate(x11, y1, z11);
-
-                Color meshColor = StdDraw.BOOK_BLUE;
-                if (n % 2 == 0) {
-                    meshColor = StdDraw.BOOK_RED;
-                }
-
-                meshes.add(new Mesh(new Coordinate[]{v00, v10, v11, v01}, meshColor));
-            }
-        }
-
-
-
-
-
-
-
-        // create bottom triangles
+        Coordinate top = new Coordinate(xPosition, yPosition + radius, zPosition);
         Coordinate bottom = new Coordinate(xPosition, yPosition - radius, zPosition);
 
-        phi = (numStacks - 1) * stackAngle;
-        distanceFromYAxis = radius * Math.sin(phi);
-        distanceFromXZPlane = radius * Math.cos(phi);
-        y = yPosition + distanceFromXZPlane;
-
+        // iterate through each slice (vertical section) and create meshes from the top down
         for (int n = 0; n < numSlices; n++) {
-            double theta = n * sliceAngle;
+            // alternate slice color to better visualize mesh
+            Color meshColor = StdDraw.BOOK_BLUE;
+            if (n % 2 == 0) {
+                meshColor = StdDraw.BOOK_RED;
+            }
+
+            double theta = n * sliceAngle; // angle parallel to the equator (longitude)
+            double phi = stackAngle; // angle parallel to y-axis (latitude)
+
+            // create top triangle
+            double distanceFromYAxis = radius * Math.sin(phi);
+            double distanceFromXZPlane = radius * Math.cos(phi);
+            double y = yPosition + distanceFromXZPlane;
 
             double x0 = xPosition + distanceFromYAxis * Math.cos(theta);
             double z0 = zPosition + distanceFromYAxis * Math.sin(theta);
@@ -119,12 +51,36 @@ public class Sphere extends Entity {
             double z1 = zPosition + distanceFromYAxis * Math.sin(theta + sliceAngle);
             Coordinate v1 = new Coordinate(x1, y, z1);
 
-            Color meshColor = StdDraw.BOOK_BLUE;
-            if (n % 2 == 0) {
-                meshColor = StdDraw.BOOK_RED;
+            meshes.add(new Mesh(new Coordinate[]{top, v0, v1}, meshColor));
+
+            // save previously computed coordinates to limit duplicates
+            Coordinate previousV0 = v0;
+            Coordinate previousV1 = v1;
+
+            // render middle quad meshes
+            for (int m = 1; m < numStacks - 1; m++) {
+                phi += stackAngle;
+
+                distanceFromYAxis = radius * Math.sin(phi);
+                distanceFromXZPlane = radius * Math.cos(phi);
+                y = yPosition + distanceFromXZPlane;
+
+                x0 = xPosition + distanceFromYAxis * Math.cos(theta);
+                z0 = zPosition + distanceFromYAxis * Math.sin(theta);
+                v0 = new Coordinate(x0, y, z0);
+
+                x1 = xPosition + distanceFromYAxis * Math.cos(theta + sliceAngle);
+                z1 = zPosition + distanceFromYAxis * Math.sin(theta + sliceAngle);
+                v1 = new Coordinate(x1, y, z1);
+
+                meshes.add(new Mesh(new Coordinate[]{previousV0, v0, v1, previousV1}, meshColor));
+
+                previousV0 = v0;
+                previousV1 = v1;
             }
 
-            meshes.add(new Mesh(new Coordinate[]{bottom, v0, v1}, meshColor));
+            // add bottom triangle
+            meshes.add(new Mesh(new Coordinate[]{previousV0, bottom, previousV1}, meshColor));
         }
     }
 
