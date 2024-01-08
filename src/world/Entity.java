a035d109999354ce02a06ec476dc38e025d4bf41
Ronnie Beggs <rbeggs@berkeley.edu>
2024-01-08 11:58:25 -0800

    [wip] incorporate yaw transformations to make entities rotatable.

--- src/world/Entity.java
+++ src/world/Entity.java
@@ -8,19 +8,31 @@ public class Entity {
     public double xPosition;
     public double yPosition;
     public double zPosition;
-    public List<Coordinate> vertices;
+    public double pitch; // vertical rotation (looking up and down)
+    public double yaw; // rotation within xz-plane (turning right and left)
+    public double roll; // rotation relative to viewing plane (barrel roles)
     public List<Mesh> meshes;
 
     public Entity(double x, double y, double z) {
         xPosition = x;
         yPosition = y;
         zPosition = z;
-    }
-
-    public List<Mesh> getMeshes() {
-        return this.meshes;
+        pitch = 0;
+        yaw = 0;
+        roll = 0;
     }
     public Coordinate getPosition() {
         return new Coordinate(xPosition, yPosition, zPosition);
+    }
+    public void setDirection(double pitch, double yaw, double roll) {
+        this.pitch = pitch;
+        this.yaw = yaw;
+        this.roll = roll;
+    }
+    public Coordinate getDirection() {
+        return new Coordinate(pitch, yaw, roll);
+    }
+    public List<Mesh> getMeshes() {
+        return this.meshes;
     }
 }
--- src/world/Spacecraft.java
+++ src/world/Spacecraft.java
@@ -11,12 +11,13 @@ public class Spacecraft extends Entity {
     public double tankLength; // length of the spacecraft tank (also determines nose length)
     public double tankRadius; // radius of the spacecraft tank
     public int numSlices; // number of mesh slices
-    public Spacecraft(double x, double y, double z, double tankLength, double tankRadius, int numSlices) {
+    public Spacecraft(double x, double y, double z, double pitch, double yaw, double roll, double tankLength, double tankRadius, int numSlices) {
         super(x, y, z);
         this.tankLength = tankLength;
         this.tankRadius = tankRadius;
         this.numSlices = numSlices;
         this.meshes = new ArrayList<>();
+        setDirection(pitch, yaw, roll);
         createMesh();
     }
     /** Create the surface mesh for the spacecraft entity. */
@@ -24,9 +25,9 @@ public class Spacecraft extends Entity {
         // save bottom coordinates to render separately at the end
         Coordinate[] bottomCoordinates = new Coordinate[numSlices];
 
-        Coordinate nose = new Coordinate(xPosition, yPosition + tankLength, zPosition);
-        Coordinate startTop = new Coordinate(xPosition + tankRadius, yPosition + (tankLength / 2), zPosition);
-        Coordinate startBottom = new Coordinate(xPosition + tankRadius, yPosition - (tankLength / 2), zPosition);
+        Coordinate nose = directionTransform(new Coordinate(xPosition, yPosition + tankLength, zPosition));
+        Coordinate startTop = directionTransform(new Coordinate(xPosition + tankRadius, yPosition + (tankLength / 2), zPosition));
+        Coordinate startBottom = directionTransform(new Coordinate(xPosition + tankRadius, yPosition - (tankLength / 2), zPosition));
         bottomCoordinates[0] = startBottom;
 
         Coordinate previousTop = startTop;
@@ -37,21 +38,26 @@ public class Spacecraft extends Entity {
         for (int n = 1; n < numSlices; n++) {
             double theta = n * sliceAngle;
 
-            Coordinate bottom = new Coordinate(
+            Coordinate bottom = directionTransform(new Coordinate(
                     xPosition + tankRadius * Math.cos(theta),
                     yPosition - (tankLength / 2),
                     zPosition + tankRadius * Math.sin(theta)
-            );
+            ));
             bottomCoordinates[n] = bottom;
 
-            Coordinate top = new Coordinate(
+            Coordinate top = directionTransform(new Coordinate(
                     xPosition + tankRadius * Math.cos(theta),
                     yPosition + (tankLength / 2),
                     zPosition + tankRadius * Math.sin(theta)
-            );
+            ));
+
+            Color meshColor = StdDraw.GRAY;
+            if (n == 1) {
+                meshColor = StdDraw.BOOK_BLUE;
+            }
 
             // create the tank mesh slice
-            meshes.add(new Mesh(new Coordinate[]{previousBottom, previousTop, top, bottom}, StdDraw.GRAY));
+            meshes.add(new Mesh(new Coordinate[]{previousBottom, previousTop, top, bottom}, meshColor));
             // create the nose mesh slice
             meshes.add(new Mesh(new Coordinate[]{previousTop, nose, top}, StdDraw.BOOK_RED));
 
@@ -63,5 +69,40 @@ public class Spacecraft extends Entity {
         meshes.add(new Mesh(new Coordinate[]{previousTop, nose, startTop}, StdDraw.BOOK_RED));
         // create mesh at the bottom of the tank
         meshes.add(new Mesh(bottomCoordinates, StdDraw.PRINCETON_ORANGE));
+    }
+
+//    public Coordinate directionTransform(Coordinate original) {
+//        double X = original.getX();
+//        double Y = original.getY();
+//        double Z = original.getZ();
+//
+//        double thetaX = -Math.toRadians(pitch); // pitch
+//        double thetaY = -Math.toRadians(yaw - 90); // yaw
+//        double thetaZ = Math.toRadians(roll); // roll
+//
+//        double dX = Math.cos(thetaY) * (Math.sin(thetaZ) * Y + Math.cos(thetaZ) * X) - Math.sin(thetaY) * Z;
+//        double dY = Math.sin(thetaX) * (Math.cos(thetaY) * Z + Math.sin(thetaY) * (Math.sin(thetaZ) * Y + Math.cos(thetaZ) * X)) + Math.cos(thetaX) * (Math.cos(thetaZ) * Y - Math.sin(thetaZ) * X);
+//        double dZ = Math.cos(thetaX) * (Math.cos(thetaY) * Z + Math.sin(thetaY) * (Math.sin(thetaZ) * Y + Math.cos(thetaZ) * X)) - Math.sin(thetaX) * (Math.cos(thetaZ) * Y - Math.sin(thetaZ) * X);
+//
+//        return new Coordinate(dX, dY, dZ);
+//    }
+
+    public Coordinate directionTransform(Coordinate realPosition) {
+        // calculate positions relative to the object's center
+        double xE = realPosition.getX() - getPosition().getX();
+        double yE = realPosition.getY() - getPosition().getY();
+        double zE = realPosition.getZ() - getPosition().getZ();
+
+        double yawRadians = Math.toRadians(yaw);
+
+        double x1 = xE * Math.cos(yawRadians) + zE * Math.sin(yawRadians);
+        double y1 = yE;
+        double z1 = -xE * Math.sin(yawRadians) + zE * Math.cos(yawRadians);
+
+        return new Coordinate(
+                x1 + getPosition().getX(),
+                y1 + getPosition().getY(),
+                z1 + getPosition().getZ()
+        );
     }
 }
